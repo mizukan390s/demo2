c631566b808990252615d470c4a5f38b460792d1
NaokiSakamoto <187244142+mizukan390s@users.noreply.github.com>
2024-11-27 11:00:51 +0900

    HTMLファイル作成完了

--- src/main/java/com/example/demo2/AdminPageController.java
+++ src/main/java/com/example/demo2/AdminPageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminPageController {
 	@GetMapping("/adminPage")
-	public String adminPage() {///ll
+	public String adminPage() {
 		return "adminPage";
 	}
 }
